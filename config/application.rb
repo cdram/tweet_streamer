ae6d25f73b0b2631f3f8790be142e8144bde3f93
shriram <shriram.r.ramamurthy@gmail.com>
2015-09-14 17:06:57 -0700

    configuring search box in app.rb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Twstream
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
+    Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
   end
 end
